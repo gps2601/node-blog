91ea8ce682083cb9a1fa6ac4fdd7a99854595142
Geoff Smith <smith260194@gmail.com>
2019-07-03 19:40:00 +0100

    Adding some logging when connecting to db

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 
 mongoose.connect(config.MONGODB_URI, {useNewUrlParser: true})
+    .then(() => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connection to MongoDB:', error.message)
+    })
+
 app.use(cors())
 app.use(bodyParser.json())
 app.use("/", blogsRouter)
