ec07a7fbaabe45bfb7554bba1bf927f249210cdb
Geoff Smith <smith260194@gmail.com>
2019-07-03 20:56:54 +0100

    Adding more tests

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,12 @@ const totalLikes = (listOfBlogs) => {
     return listOfBlogs.reduce((a,b) => a + b.likes, 0)
 }
 
+const favouriteBlog = (listOfBlogs) => {
+    return listOfBlogs.reduce((prev, curr) => prev.likes > curr.likes ? prev : curr)
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favouriteBlog
 }
